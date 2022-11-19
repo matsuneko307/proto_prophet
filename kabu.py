a7ac3974ae7437d7960b483b15602b31091d90e3
matsuneko307 <112343365+matsuneko307@users.noreply.github.com>
2022-11-19 16:03:48 +0900

    Update kabu.py

--- kabu.py
+++ kabu.py
@@ -8,53 +8,53 @@ from datetime import datetime
 
 code = st.text_input('米国株のティッカーシンボルを1銘柄だけ半角で入力、、Enter を押してください')
 st.write("例:Amazonなら、AMZN と入力"
+if code:
+  date_st = datetime(2010, 1, 1)
+  date_fn = datetime(2021, 4, 1)
 
-date_st = datetime(2010, 1, 1)
-date_fn = datetime(2021, 4, 1)
+  df1 = YahooDailyReader(code, date_st, date_fn).read()
 
-df1 = YahooDailyReader(code, date_st, date_fn).read()
+  symbols = [code]
 
-symbols = [code]
+  dfs = [YahooDailyReader(symbol, date_st, date_fn).read() for symbol in symbols]
 
-dfs = [YahooDailyReader(symbol, date_st, date_fn).read() for symbol in symbols]
+  dfs = []
+  for symbol in symbols:
+    df = YahooDailyReader(symbol, date_st, date_fn).read()
+    dfs.append(df)
 
-dfs = []
-for symbol in symbols:
-  df = YahooDailyReader(symbol, date_st, date_fn).read()
-  dfs.append(df)
+  df2 = pd.concat(dfs, axis=0, keys=symbols).unstack(0)
 
-df2 = pd.concat(dfs, axis=0, keys=symbols).unstack(0)
+  from prophet import Prophet
 
-from prophet import Prophet
+  data = df1.reset_index().rename(columns={'Date': 'ds', 'Close': 'y'})
 
-data = df1.reset_index().rename(columns={'Date': 'ds', 'Close': 'y'})
+  # インスタンス化
+  model = Prophet()
 
-# インスタンス化
-model = Prophet()
+  # 学習
+  model.fit(data)
 
-# 学習
-model.fit(data)
+  # 学習データに基づいて未来を予測
+  future = model.make_future_dataframe(periods=365)
+  forecast = model.predict(future)
 
-# 学習データに基づいて未来を予測
-future = model.make_future_dataframe(periods=365)
-forecast = model.predict(future)
+  # 予測結果の可視化
+  #model.plot(forecast)
+  #plt.show()
 
-# 予測結果の可視化
-#model.plot(forecast)
-#plt.show()
-
-# トレンド性と周期性の抽出
-#model.plot_components(forecast)
-#plt.show()
-
-
-import streamlit as st
-# markdownで文章が書ける
-st.markdown('# 株価予測')
-fig1 = model.plot(forecast)
-st.pyplot(fig1)
+  # トレンド性と周期性の抽出
+  #model.plot_components(forecast)
+  #plt.show()
 
 
-st.markdown('# トレンド性・周期性の抽出')
-fig2 = model.plot_components(forecast)
-st.pyplot(fig2)
+  import streamlit as st
+  # markdownで文章が書ける
+  st.markdown('# 株価予測')
+  fig1 = model.plot(forecast)
+  st.pyplot(fig1)
+
+
+  st.markdown('# トレンド性・周期性の抽出')
+  fig2 = model.plot_components(forecast)
+  st.pyplot(fig2)
